9bc5e6ece6475e2b5c975bd14c73ffc8ee9c72f8
webrectoversocreationbe <web@rectoversocreation.be>
2013-10-07 17:16:05 +0200

    re

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		alert($('#result').html());
+		alert($('#A').val());
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
